7245ef83139cd575707e354108e8e5d521381274
py660 <93660666+py660@users.noreply.github.com>
2024-06-10 17:07:37 -0400

    fix page-unfocused glitch

--- script.js
+++ script.js
@@ -129,7 +129,9 @@ let mainloop = setInterval(()=>{
                     ctx.fillText(timetext, width/2 + 35, height/2 - 50 + 25);
                 }
                 else{
-                    countdown--;
+                    if (document.hasFocus()){
+                        countdown--;
+                    }
                 }
             }, 1000);
             setTimeout(spawn, 700);
@@ -137,79 +139,81 @@ let mainloop = setInterval(()=>{
         }
     }
     else{
-        ctx.clearRect(0, 0, width, height);
-        ctx.fillStyle = "rgb(108, 108, 118)";
-        //ctx.fillRect(0, height - height*power, width, height*power);
+        if (document.hasFocus()){
+            ctx.clearRect(0, 0, width, height);
+            ctx.fillStyle = "rgb(108, 108, 118)";
+            //ctx.fillRect(0, height - height*power, width, height*power);
 
-        y = Math.min(height-10-size/2, Math.max(10+size/2, height - height*power)) - size/2
+            y = Math.min(height-10-size/2, Math.max(10+size/2, height - height*power)) - size/2
 
-        let newsprites = [];
-        for (let sprite of sprites){
-            if (sprite.dest[0]-x+sprite.dest[2] < 0){
-                continue;
-            }
-            /*ctx.fillStyle = "red";
-            ctx.fillRect(sprite.dest[0]-x, sprite.dest[1], 5, 5);
-            ctx.fillRect(10+size, y + size, 5, 5);
-            ctx.fillStyle = "green";
-            ctx.fillRect(sprite.dest[0]-x, sprite.dest[1]+sprite.dest[3], 5, 5);
-            ctx.fillRect(10+size, y, 5, 5);*/
-            if (sprite.type == 2){
-                sprite.dest[0] -= 1;
-            }
-            if (sprite.type == 1){
-                sprite.dest[0] -= 0.5;
-            }
-            if (eaten || ! (sprite.dest[0]-x < 10+size && 10+size < sprite.dest[0]-x+20 && sprite.dest[1] < y + size && sprite.dest[1] + sprite.dest[3] > y)){
-                newsprites.push(sprite);
-                ctx.drawImage([img.tiny, img.big, img.shark][sprite.type], ...sprite.source, sprite.dest[0]-x, ...sprite.dest.slice(1));
-            }
-            else if (sprite.type == 2){
-                eaten = true;
-                clearTimeout(eatenTimeout);
-                eatenTimeout = setTimeout(()=>{eaten = false}, 2000);
-                points -= 45;
-                modifiers.push({type: 2, x: x, y: sprite.dest[1]});
-            }
-            else{
-                eating = true;
-
+            let newsprites = [];
+            for (let sprite of sprites){
+                if (sprite.dest[0]-x+sprite.dest[2] < 0){
+                    continue;
+                }
+                /*ctx.fillStyle = "red";
+                ctx.fillRect(sprite.dest[0]-x, sprite.dest[1], 5, 5);
+                ctx.fillRect(10+size, y + size, 5, 5);
+                ctx.fillStyle = "green";
+                ctx.fillRect(sprite.dest[0]-x, sprite.dest[1]+sprite.dest[3], 5, 5);
+                ctx.fillRect(10+size, y, 5, 5);*/
+                if (sprite.type == 2){
+                    sprite.dest[0] -= 1;
+                }
                 if (sprite.type == 1){
-                    points += 60;
-                    modifiers.push({type: 3, x: x, y: sprite.dest[1]});
+                    sprite.dest[0] -= 0.5;
                 }
-                if (sprite.type == 0){
-                    points += 30;
-                    modifiers.push({type: 1, x: x, y: sprite.dest[1]});
+                if (eaten || ! (sprite.dest[0]-x < 10+size && 10+size < sprite.dest[0]-x+20 && sprite.dest[1] < y + size && sprite.dest[1] + sprite.dest[3] > y)){
+                    newsprites.push(sprite);
+                    ctx.drawImage([img.tiny, img.big, img.shark][sprite.type], ...sprite.source, sprite.dest[0]-x, ...sprite.dest.slice(1));
                 }
-                
-                clearTimeout(eatTimeout);
-                eatingTimeout = setTimeout(()=>{eating = false}, 500);
+                else if (sprite.type == 2){
+                    eaten = true;
+                    clearTimeout(eatenTimeout);
+                    eatenTimeout = setTimeout(()=>{eaten = false}, 2000);
+                    points -= 45;
+                    modifiers.push({type: 2, x: x, y: sprite.dest[1]});
+                }
+                else{
+                    eating = true;
+
+                    if (sprite.type == 1){
+                        points += 60;
+                        modifiers.push({type: 3, x: x, y: sprite.dest[1]});
+                    }
+                    if (sprite.type == 0){
+                        points += 30;
+                        modifiers.push({type: 1, x: x, y: sprite.dest[1]});
+                    }
+                    
+                    clearTimeout(eatTimeout);
+                    eatingTimeout = setTimeout(()=>{eating = false}, 500);
+                }
             }
-        }
-        sprites = newsprites;
+            sprites = newsprites;
 
-        newmodifiers = [];
-        for (let modifier of modifiers){
-            if (modifier.x > x - 100){
-                newmodifiers.push(modifier);
-                ctx.drawImage([0, img.p30, img.p45, img.p60][modifier.type], 100, modifier.y, 70, 70)
+            newmodifiers = [];
+            for (let modifier of modifiers){
+                if (modifier.x > x - 100){
+                    newmodifiers.push(modifier);
+                    ctx.drawImage([0, img.p30, img.p45, img.p60][modifier.type], 100, modifier.y, 70, 70)
+                }
             }
+            modifiers = newmodifiers;
+
+            ctx.drawImage(eaten ? img.eaten.image : eating ? img.eat : img.normal, 2, 7, 29, 20, 10, y, 30/25*size, size);
+            x += 2;
+
+            points = Math.max(0, points);
+            ctx.fillStyle = "black";
+            ctx.font = "50px Courier New, monospace";
+            ctx.fillText(points + " pts",10,60);
+
+            ctx.fillStyle = "black";
+            ctx.font = "50px Courier New, monospace";
+            let timeString = `${Math.floor(countdown/60).toString().padStart(2, "0")}:${(countdown%60).toString().padStart(2, "0")}`;
+            ctx.fillText(timeString, width - ctx.measureText(timeString).width - 10, 60);
         }
-        modifiers = newmodifiers;
-
-        ctx.drawImage(eaten ? img.eaten.image : eating ? img.eat : img.normal, 2, 7, 29, 20, 10, y, 30/25*size, size);
-        x += 2;
-
-        points = Math.max(0, points);
-        ctx.fillStyle = "black";
-        ctx.font = "50px Courier New, monospace";
-        ctx.fillText(points + " pts",10,60);
-
-        ctx.fillStyle = "black";
-        ctx.font = "50px Courier New, monospace";
-        let timeString = `${Math.floor(countdown/60).toString().padStart(2, "0")}:${(countdown%60).toString().padStart(2, "0")}`;
-        ctx.fillText(timeString, width - ctx.measureText(timeString).width - 10, 60);
     }
 }, 10);
 
